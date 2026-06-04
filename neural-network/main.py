d7b1c79d30bd392847f3571c2b501df6bde2297f
jimmy <jimoseipeace2bb@gmail.com>
2026-06-04 12:11:14 -0500

    Done with the mnist neural project. Learned alot with the two layer neural net. I need to review the shapes of the matrix to fully grasp it

--- neural-network/main.py
+++ neural-network/main.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from keras.datasets import mnist
-from net_model import (one_hot)
+from net_model import (one_hot, mini_batch_gradient,prediction,accuracy,forward_prop)
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
@@ -18,17 +18,33 @@ from net_model import (one_hot)
 X_train = X_train.reshape(60000,784) /255.0
 X_test = X_test.reshape(10000,784) /255.0
 
-#Transform the mattrix
-X_train = X_train.T
+#Transform the matrix
+X_train = X_train.T # our neural net expect each column = one image
 X_test = X_test.T
 # print(X_train.shape) #(784, 60000)
 # print(X_test.shape) #(784, 10000)
 # print(X_train[:5, :3])
-
+#preprocess y
 Y_train = one_hot(y_train)
 Y_test  = one_hot(y_test)
+# print(y_train[0])
+# print(Y_train[:, 0])
+# print(Y_train.shape)
 
-print(y_train[0])
-print(Y_train[:, 0])
+#train
+W1,b1,W2,b2 = mini_batch_gradient(X_train,Y_train, B=64, alpha=0.1,num_iteration=10)
 
 
+_, _, _, A2_train = forward_prop(W1,b1,W2,b2,X_train)
+train_predictions = prediction(A2_train)
+train_accuracy = accuracy(train_predictions ,y_train)
+
+_, _, _, A2_test = forward_prop(W1,b1,W2,b2,X_test)
+test_prediction = prediction(A2_test)
+test_accuracy = accuracy(test_prediction, y_test)
+
+print("Train accuracy: ", train_accuracy)
+print("Test accuracy:", test_accuracy)
+
+print("First 10 predictions: ", test_prediction[:10])
+print("First 10 labels: ", y_test[:10])
--- neural-network/net_model.py
+++ neural-network/net_model.py
@@ -4,21 +4,17 @@ import numpy as np
 # 60000 = number of training images
 # 10 = possible digit labels/classes
 
+#After training we ask ourselves these questions
+# 1. What digit does the model predict?
+# 2. How often is it correct?
+
 def one_hot(y, num_classes = 10):
     one_hot_y = np.zeros((num_classes, y.size))
     one_hot_y[y,np.arange(y.size)] = 1
     return one_hot_y
 
 
-def init_params():
-#0.01 makes the weight small
-    hidden_size = 128
-    W1 = np.random.randn(hidden_size, 60000) * 0.01
-    b1 = np.zeros((hidden_size, 1))
-    W2 = np.random.randn(10,hidden_size) * 0.01
-    b2 = np.zeros((10,1))
 
-    return  W1, b1, W2, b2
 
 def RELU(z):
     return np.maximum(0, z)
@@ -29,15 +25,32 @@ def softmax(z):
     return exp_z / np.sum(exp_z, axis=0, keepdims=True)
 
 
-def forward_prop(W1,b1,W2,b2,X):
-    Z1 = W1.dot(X) + b1
-    A1 = RELU(Z1)
-    Z2 = W2.dot(A1)  + b2
-    A2 = softmax(Z2)
-    return Z1, A1, Z2, A2
+
+def prediction(A2):
+    return np.argmax(A2, axis=0)
+
+def accuracy(predictions,y):
+    return np.mean(predictions == y)
 
 def relu_derivative(z):
     return z > 0
+
+def init_params():
+#0.01 makes the weight small
+    hidden_size = 128
+    W1 = np.random.randn(hidden_size, 784) * 0.01
+    b1 = np.zeros((hidden_size, 1))
+    W2 = np.random.randn(10,hidden_size) * 0.01
+    b2 = np.zeros((10,1))
+
+    return  W1, b1, W2, b2
+
+def forward_prop(W1,b1,W2,b2,X):
+    Z1 = W1.dot(X) + b1
+    A1 = RELU(Z1)
+    Z2 = W2.dot(A1) + b2
+    A2 = softmax(Z2)
+    return Z1, A1, Z2, A2
 
 def backprop(Z1,A1,Z2,A2,W2,X,Y):
     m = Y.shape[1]
@@ -46,6 +59,44 @@ def backprop(Z1,A1,Z2,A2,W2,X,Y):
     dW2 = (1 / m) * dZ2  @ A1.T
     db2 =  (1 / m ) * np.sum(dZ2, axis=1, keepdims=True)
     dZ1 = W2.T @dZ2 *relu_derivative(Z1)
-    dW1 = (1 / m ) * dZ1 * X.T
+    dW1 = (1 / m ) * dZ1 @ X.T
     db1 = (1/m)  * np.sum(dZ1, axis = 1, keepdims=True)
 
+    return dW1, db1, dW2, db2
+
+
+# meaning
+# if gradient is positive, subtracting moves the weight down
+# if gradient is negative, subtracting moves the weight up
+
+def update_params(W1,b1,W2, b2, dW1,db1,dW2,db2, alpha):
+    W1 = W1 - (alpha * dW1)
+    b1 = b1 - (alpha * db1)
+    W2 = W2 - (alpha * dW2)
+    b2 = b2 - (alpha * db2)
+
+    return W1, b1, W2, b2
+
+
+def mini_batch_gradient(X,Y , B, alpha, num_iteration=10):
+    W1,b1,W2,b2 = init_params()
+    m = X.shape[1]
+
+    for epoch in range(num_iteration):
+        permutation = np.random.permutation(m)
+
+        X_shuffled = X[:, permutation]
+        Y_shuffled = Y[:, permutation]
+
+        for start in range(0,m , B):
+            end = start + B
+            X_batch = X_shuffled[:, start:end]
+            Y_batch = Y_shuffled[:, start:end]
+            Z1, A1, Z2 , A2 = forward_prop(W1,b1,W2,b2,X_batch)
+            dW1,db1,dW2,db2 = backprop(
+                Z1, A1, Z2, A2, W2, X_batch,Y_batch )
+
+            W1,b1,W2,b2 = update_params(W1,b1,W2,b2, dW1,db1,dW2,db2, alpha)
+
+
+    return W1,b1,W2,b2
